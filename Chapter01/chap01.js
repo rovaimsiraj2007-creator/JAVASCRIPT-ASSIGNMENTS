11b9ccf2a755ac991387210a0baa489125df458e
rovaimsiraj2007-creator <rovaimsiraj2007@gmail.com>
2026-01-04 21:45:10 +0500

    Add files via upload

---
+++ Chapter01/chap01.js
@@ -0,0 +1,16 @@
+// Q # 01
+alert("Question 01");
+
+// Q # 02
+alert("Question 02 \nError! Please enter a valid password");
+
+// Q # 03
+alert("Question 03 \nWelcome to JS Land... \nHappy Coding!");
+
+// Q # 04
+alert("Question 04 \nWelcome to JS Land...");
+alert("Happy Coding! \nPrevent this page from creating additional dialogs.");
+
+// Q # 05
+// alert();
+console.log("Question 05 \nHello... I can run JS through my web browser's console");
---
+++ Chapter02/chap02.js
@@ -0,0 +1,40 @@
+// Question 01
+let username = "username";
+
+// Question 02
+let myName = "Rovaim Siraj";
+
+// Question 03
+var message;
+message = "Hello World";
+alert("Question 03 \n" + message);
+
+// Question 04
+var studentName = "Rovaim Siraj";
+var age = 18;
+var course = "Web Development";
+var institute = "Saylani mass IT training center"
+
+alert("Question 04 \nSutudent Name: " + studentName);
+alert("Age: " + age);
+alert("Course: "+ course);
+alert("Institute: "+ institute);
+
+//Question 05
+var food = "Question 05 \nPIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(food);
+
+//Question 06
+var email = "rovaimsiraj2007@gmil.com";
+alert("Question 06 \nMy email address is " + email);
+
+//Question 07
+var book = "A smarter way to learn JavaScript";
+alert("Question 07 \nI am trying to learn from the book "+ book);
+
+// Question 08
+alert("Question 08 \nYah! I can write HTML content through JavaScript");
+
+//Question 09
+var design = "Question 09 \n--------------ஜ۩۞۩ஜ---------------"
+alert(design);
