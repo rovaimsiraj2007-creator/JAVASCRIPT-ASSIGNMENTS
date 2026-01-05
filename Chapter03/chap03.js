4edb8c0f0ac5cc62cf88651e394baf4e0fac2713
rovaimsiraj2007-creator <rovaimsiraj2007@gmail.com>
2026-01-05 19:39:28 +0500

    Update chap03.js

--- Chapter03/chap03.js
+++ Chapter03/chap03.js
@@ -5,3 +5,15 @@ alert("Question 01 \nI am " + age + " years old");
 }
 
 //Question 02
+function ques02(){
+let visitCount = 1;
+alert("You have visited this site "+  visitCount + "times" );
+}
+
+//Question 04
+function ques04(){
+var Visitor_Name = "John Doe"
+var Quantity = 5
+var Product_Name = "T_Shirt"
+alert(Visitor_Name + " Order " +  Quantity + "" + Product_Name+ "(s) on XYZ Clothing store")
+}
