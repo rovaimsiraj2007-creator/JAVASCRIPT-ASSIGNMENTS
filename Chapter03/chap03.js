33d9d3de6bd7cec1075f52c86c8f42e495f31741
rovaimsiraj2007-creator <rovaimsiraj2007@gmail.com>
2026-01-06 20:57:03 +0500

    chap 4 compelete

--- Chapter03/chap03.js
+++ Chapter03/chap03.js
@@ -1,19 +1,27 @@
 //Question 01
-function ques01(){
-var age = 18;
-alert("Question 01 \nI am " + age + " years old");
+function ques01() {
+    var age = 18;
+    alert("Question 01 :\nI am " + age + " years old");
 }
 
 //Question 02
-function ques02(){
-let visitCount = 1;
-alert("You have visited this site "+  visitCount + "times" );
+function ques02() {
+    let visitCount = 1;
+    alert("Question 02 : \nYou have visited this site " + visitCount + "times");
+}
+
+// Q # 03
+function ques03() {
+    let birthYear = 2007
+    document.write("Question 03 :" + "<br>")
+    document.write("My birth year is " + birthYear + "<br>");
+    document.write("Data type of my Declerad variable is Number");
 }
 
 //Question 04
-function ques04(){
-var Visitor_Name = "John Doe"
-var Quantity = 5
-var Product_Name = "T_Shirt"
-alert(Visitor_Name + " Order " +  Quantity + "" + Product_Name+ "(s) on XYZ Clothing store")
+function ques04() {
+    var Visitor_Name = "John Doe"
+    var Quantity = 5
+    var Product_Name = "T Shirts"
+    alert( "Question 04 : \n" + Visitor_Name + " Order " + Quantity  + " " + Product_Name + "  on XYZ Clothing store")
 }
---
+++ Chapter05/chap05.js
@@ -0,0 +1,32 @@
+// Q # 01
+function ques01(){
+     // Take two numbers
+    let num1 = 10;
+    let num2 = 20;
+
+    // Add them and store in a new variable
+    let result = num1 + num2;
+
+    // Show the result in the browser
+    alert("Question 01 \nThe sum 10 and 20 is : " + result);
+}
+
+// Q # 02
+function ques02(){
+    // Take two numbers
+    let num1 = 20;
+    let num2 = 6;
+
+    // Perform operations
+    let subtraction = num1 - num2;
+    let multiplication = num1 * num2;
+    let division = num1 / num2;
+    let modulus = num1 % num2;
+
+    // Display results in browser
+    document.write("Question 02" + "<br>")
+    document.write("Subtraction: " + subtraction + "<br>");
+    document.write("Multiplication: " + multiplication + "<br>");
+    document.write("Division: " + division + "<br>");
+    document.write("Modulus: " + modulus);
+}
