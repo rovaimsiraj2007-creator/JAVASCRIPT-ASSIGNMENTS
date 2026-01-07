74fa39c07d6ae73e7b6bf3361cadc405474de045
rovaimsiraj2007-creator <rovaimsiraj2007@gmail.com>
2026-01-07 08:44:17 +0500

    chap 5 q5

--- Chapter05/chap05.js
+++ Chapter05/chap05.js
@@ -30,3 +30,48 @@ function ques02(){
     document.write("Division: " + division + "<br>");
     document.write("Modulus: " + modulus);
 }
+
+function ques03(){
+let a;
+document.write("Value after variable declaration is: " + a + "<br>");
+
+a = 5;
+document.write("Initial value: " + a + "<br>");
+
+a++;
+document.write("Value after increment is: " + a + "<br>");
+
+a = a + 7;
+document.write("Value after addition is: " + a + "<br>");
+
+a--;
+document.write("Value after decrement is: " + a + "<br>");
+
+let remainder = a % 3;
+
+document.write("The remainder is: " + remainder + "<br><br>");
+}
+
+function ques04(){
+let ticketprice = 600;
+let totaltickets = 5;
+let totalcost = ticketprice * totaltickets ;
+
+document.write("Cost of one movie ticket is " + ticketprice + "PKR" +"<br>");
+document.write("Costumer buying " + totaltickets + " tikets" +"<br>");
+document.write("Total cost to buy " + totaltickets + " tickets to a movie is " + totalcost + "PKR");
+}
+
+function ques05(){
+
+let Tableprint = prompt("Which table do you want to print?");
+let Limit = prompt("Up to which number do you want to print it?");
+alert("Your table has been printed on your browser!")
+
+document.write("Table of " + Tableprint + "<br>")
+for(let i = 1; i <= Limit; i++){
+    document.write(Tableprint + " x " + i + " = " + i * Tableprint + "<br>");
+    
+}
+
+}
